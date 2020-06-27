538b603c9584cc32e0ed0ece80a3d22999898c02
YukoSky <daffetyxd@gmail.com>
2020-06-27 20:12:59 -0300

    make.sh: output & others: some changes

--- make.sh
+++ make.sh
@@ -242,6 +242,7 @@ if [ "$sourcever" == "9" ]; then
     useold="--old"
 fi
 $scriptsdir/mkimage.sh $systemdir $outputtype $systemsize $output $useold > $tempdir/mkimage.log
+echo "-> Created image ($outputtype): $outputimagename | Size: $systemsize"
 
-echo "- > Remove Temp dir"
+echo "-> Remove Temp dir"
 rm -rf "$tempdir"
