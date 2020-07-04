f7f87df247b90fbc9ced249fdf19adff1ea75b98
ゆうこスカイ ~#YuMi1621 <57292142+yukosky@users.noreply.github.com>
2020-07-04 10:21:47 -0300

    getinfo.sh: tmp to cache dir

--- runner/getinfo.sh
+++ runner/getinfo.sh
@@ -2,9 +2,9 @@
 if grep -q ro.build.version.release_or_codename $systemdir/system/build.prop; then
     export sourcever2=`grep ro.build.version.release_or_codename $systemdir/system/build.prop | cut -d "=" -f 2`
     touch ../tmp/ver
-    echo "$sourcever2" >> ../tmp/ver
+    echo "$sourcever2" >> ../cache/ver
 else
     export sourcever2=`grep ro.build.version.release $systemdir/system/build.prop | cut -d "=" -f 2`
     touch ../tmp/ver
-    echo "$sourcever2" >> ../tmp/ver
+    echo "$sourcever2" >> ../cache/ver
 fi
