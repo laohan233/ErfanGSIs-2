02ebbd7228775f4fa7510d8a08d09c06c32885ec
YukoSky <daffetyxd@gmail.com>
2020-07-10 20:19:05 -0300

    evox 10: added more debloat

--- roms/10/EvolutionX/debloat.sh
+++ roms/10/EvolutionX/debloat.sh
@@ -11,9 +11,6 @@ rm -rf $1/priv-app/CNEService
 rm -rf $1/priv-app/DMService
 rm -rf $1/priv-app/VzwOmaTrigger
 rm -rf $1/priv-app/qcrilmsgtunnel
-
-# SUPER DEBLOAT
-
 rm -rf $1/product/app/CalculatorGooglePrebuilt
 rm -rf $1/product/app/CalendarGooglePrebuilt
 rm -rf $1/product/app/Camera2
@@ -28,3 +25,21 @@ rm -rf $1/product/app/Velvet
 rm -rf $1/product/app/TurboPrebuilt
 rm -rf $1/product/app/TipsPrebuilt
 rm -rf $1/product/app/WellbeingPrebuilt
+
+# SUPER DEBLOAT
+rm -rf $1/product/app/GoogleContactsSyncAdapter
+rm -rf $1/product/app/PixelSetupWizardAodOverlay
+rm -rf $1/product/app/AndroidMigratePrebuilt
+rm -rf $1/product/app/AndroidPlatformServices
+rm -rf $1/product/app/CarrierServices
+rm -rf $1/product/app/CarrierUpdater
+rm -rf $1/product/app/GoogleContacts
+rm -rf $1/product/app/GoogleFeedback
+rm -rf $1/product/app/GoogleOneTimeInitializer
+rm -rf $1/product/app/GooglePatnerSetup
+rm -rf $1/product/app/GoogleServicesFramework
+rm -rf $1/product/app/Phonesky
+rm -rf $1/product/app/PixelSetupWizard
+rm -rf $1/product/app/PrebuiltGmsCoreQt
+rm -rf $1/product/app/SafetyHubPrebuilt
+rm -rf $1/product/app/SetupWizardPrebuilt
