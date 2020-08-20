d25abddf5e1c2806ae44ee3097f6706ac3ce1628
YukoSky <daffetyxd@gmail.com>
2020-08-20 09:19:35 -0300

    roms: 10: fix de-bloat

--- roms/10/AncientOS/debloat.sh
+++ roms/10/AncientOS/debloat.sh
@@ -16,3 +16,17 @@ rm -rf $1/product/priv-app/EuiccSupportPixel
 rm -rf $1/product/app/Music2
 rm -rf $1/product/app/Photos
 rm -rf $1/product/app/Videos
+rm -rf $1/product/app/CalculatorGooglePrebuilt
+rm -rf $1/product/app/CalendarGooglePrebuilt
+rm -rf $1/product/app/Camera2
+rm -rf $1/product/app/GoogleTTS
+rm -rf $1/product/app/LocationHistoryPrebuilt
+rm -rf $1/product/app/MarkupGoogle
+rm -rf $1/product/app/MicropaperPrebuilt
+rm -rf $1/product/app/Photos
+rm -rf $1/product/app/PrebuiltDeskClockGoogle
+rm -rf $1/product/app/PrebuiltBugle
+rm -rf $1/product/app/Velvet
+rm -rf $1/product/app/TurboPrebuilt
+rm -rf $1/product/app/TipsPrebuilt
+rm -rf $1/product/app/WellbeingPrebuilt
--- roms/10/EvolutionX/make.sh
+++ roms/10/EvolutionX/make.sh
@@ -3,6 +3,5 @@
 systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
-# Append file_context
 echo "ro.setupwizard.mode=DISABLED" >> $1/etc/prop.default
 echo "qemu.hw.mainkeys=0" >> $1/etc/prop.default
